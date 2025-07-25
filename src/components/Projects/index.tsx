cd5eadf971c611a4accaedc96d3811d4c10f098a
Zach Shearer <shearertzach@gmail.com>
2025-07-24 23:01:53 -0500

    fixed font sizes

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -9,10 +9,10 @@ const Projects = ({setModal}: {setModal: React.Dispatch<React.SetStateAction<Pro
                 <div key={project.id} onClick={() => setModal(project)}>
                     <div className="w-full aspect-[1.5/1] bg-[#d9d9d9] mb-4 rounded-md" />
                     <div className="p-2 cursor-pointer">
-                        <p className="mb-2 text-[8.12px] md:text-[16px] font-quicksand !font-[500]">{project.projectType} - {project.projectName}</p>
-                        <h4 className="text-[14.21px] md:text-[28px] font-[800] mb-2 ">{project.title}</h4>
-                        <p className="mb-10 text-[10.15px] md:text-[20px] !font-[300] font-quicksand">{project.description}</p>
-                        <p className="font-quicksand text-[8.12px] md:text-[16px] !font-[500]">{project.dateInformation.startDate} - {project.dateInformation.endDate} | Role: {project.role}</p>
+                        <p className="mb-2 text-[10px] md:text-[16px] font-quicksand !font-[500]">{project.projectType} - {project.projectName}</p>
+                        <h4 className="text-[16px] md:text-[28px] font-[800] mb-2 ">{project.title}</h4>
+                        <p className="mb-10 text-[12px] md:text-[20px] !font-[300] font-quicksand">{project.description}</p>
+                        <p className="font-quicksand text-[10px] md:text-[16px] !font-[500]">{project.dateInformation.startDate} - {project.dateInformation.endDate} | Role: {project.role}</p>
                     </div>
                 </div>
             ))}
